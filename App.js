d27b44183da7577bbb66ab0af4e20613a9177bae
Gregor Lennie <gregorlennie@hotmail.co.uk>
2021-12-05 13:17:55 +0000

    Update output to use alert dialog

--- App.js
+++ App.js
@@ -1,51 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, StyleSheet, Text, View } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Button, Input } from 'react-native-elements';
 import { binaryToDecimal } from './util/binaryToDecimal';
 
-const regex = /^[0-1]$/;
-
 export default function App() {
-  const [binaryInput, setBinaryInput] = useState();
-  const [result, setResult] = useState();
-  const [error, setError] = useState(false);
+  const [binaryInput, setBinaryInput] = useState('');
+
+  const handleChangeText = (text) => {
+    setBinaryInput(text);
+  };
+
+  const handleCalculate = () => {
+    const result = binaryToDecimal(binaryInput).toString();
+    Alert.alert('Result', `${binaryInput} = ${result}`, [
+      {
+        text: 'OK',
+        onPress: () => {
+          setBinaryInput('');
+        },
+      },
+    ]);
+  };
 
   return (
     <SafeAreaProvider>
       <View style={styles.container}>
         <Input
-          onChangeText={(text) => {
-            setBinaryInput(text);
-          }}
+          onChangeText={handleChangeText}
           placeholder={'Enter a binary number'}
           maxLength={8}
           value={binaryInput}
           keyboardType={'number-pad'}
-          errorMessage={error && 'Input can only be 0 or 1'}
         />
-        <Button
-          disabled={!binaryInput || !regex.test(binaryInput)}
-          title={'Calculate'}
-          onPress={() => {
-            const result = binaryToDecimal(binaryInput);
-            setResult(result);
-          }}
-        />
-
-        {result && (
-          <View>
-            <Text>Result: {result}</Text>
-            <Button
-              title={'Reset'}
-              onPress={() => {
-                setResult();
-                setBinaryInput();
-              }}
-            />
-          </View>
-        )}
+        <Button title={'Calculate'} onPress={handleCalculate} />
 
         <StatusBar style="auto" />
       </View>
